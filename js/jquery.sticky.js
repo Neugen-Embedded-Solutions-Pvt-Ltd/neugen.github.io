4ad3edd18ea86f5acf429e20efd5ee9a99562093
manoj <manojarun4820@gmail.com>
2024-11-27 12:49:19 +0530

    upadte

--- js/jquery.sticky.js
+++ js/jquery.sticky.js
@@ -52,7 +52,7 @@
           etse = elementTop - s.topSpacing - extra;
 
         //update height in case of dynamic content
-        s.stickyWrapper.css('height', s.stickyElement.outerHeight());
+        s.stickyWrapper.css('height', 80);
 
         if (scrollTop <= etse) {
           if (s.currentTop !== null) {
